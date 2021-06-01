34c57edb3c56a1c37f969c2f68ce324081d86b21
GabrielTonhatti <gabrieltonhatti37@gmail.com>
2021-06-01 17:51:34 -0300

    Estudos DevMedia

---
+++ TypeScript/introducao/hello.js
@@ -0,0 +1,5 @@
+// Variável mensagem com tipo definido (string)
+var mensagem = 'Hello World!';
+// console.log recebendo como parâmentro a variável 'mensagem'.
+// Para exibir o texto associado a ela no console do navegador.
+console.log(mensagem);
---
+++ TypeScript/introducao/hello.ts
@@ -0,0 +1,8 @@
+// Variável mensagem com tipo definido (string)
+
+let mensagem:string = 'Hello World!'
+
+// console.log recebendo como parâmentro a variável 'mensagem'.
+// Para exibir o texto associado a ela no console do navegador.
+
+console.log(mensagem);
